263c083ad3273143029819529575b6e6b2117ba7
CalvinEM2020 <68593970+CalvinEM2020@users.noreply.github.com>
2020-08-11 11:49:33 +0000

    CalvinAssign3

---
+++ src/css/bigtext.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './components.css'
+function Bigtext() {
+    return (
+        <div>
+            <h1 className=''>Marvel's Fearless</h1>
+            <p className='subtitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. beatae molestias earum eveniet.</p>
+            <br />
+            <div className='search-bar'>
+            <input type='search' placeholder='search' />
+
+            </div>
+        </div>
+    )
+}
+
+export default Bigtext;
